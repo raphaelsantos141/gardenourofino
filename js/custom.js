c1e41be5ca464ba4f48c5e3039049bb7f4cb2a8c
Raphael <raphael.asantos@outlook.com>
2024-10-01 16:22:58 -0300

    correção galeria

--- js/custom.js
+++ js/custom.js
@@ -121,14 +121,7 @@ $(function () {
 				}
 			}
 		});
-		owl.on('mousewheel', '.owl-stage', function (e) {
-			if (e.deltaY > 0) {
-				owl.trigger('next.owl');
-			} else {
-				owl.trigger('prev.owl');
-			}
-			e.preventDefault();
-		});
+	
 	});
 
 	/* Scroll to Top
